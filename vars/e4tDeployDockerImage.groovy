bbcb078144f1119ccf109dd46a36cdd093174dbb
Antonio Locane <a.locane@eco-mind.eu>
2020-10-09 17:51:53 +0200

    added deploy

---
+++ vars/e4tDeployDockerImage.groovy
@@ -0,0 +1,9 @@
+void call(String repoName, String localTag, String remoteTag, String remoteHost) {
+    sh(script: """
+        /bin/bash -c ' \\
+            . /app/dep-bootstrap.sh ;\\
+            dep include log2/shell-common 0.1.0-SNAPSHOT projects ;\\
+            docker_push $repoName $localTag $remoteTag, $remoteHost ;\\
+        '
+    """)
+}
