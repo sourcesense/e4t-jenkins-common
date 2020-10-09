07a6ef98ee21f35102fbe15e8a7e3167667238e5
Antonio Locane <a.locane@eco-mind.eu>
2020-10-09 17:00:55 +0200

    added check

---
+++ vars/e4tCheckShellScripts.groovy
@@ -0,0 +1,9 @@
+void call(String baseDir='.', String reportFileName='checkstyle-report.xml') {
+    sh(script: """
+        /bin/bash -c ' \\
+            . /app/dep-bootstrap.sh ;\\
+            dep include log2/shell-common 0.1.0-SNAPSHOT check ;\\
+            check_shell_script $baseDir $reportFileName ;\\
+        '
+    """)
+}
