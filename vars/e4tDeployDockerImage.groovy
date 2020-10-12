2658ab8bb047823d72cbc6358856116fdde78c80
Antonio Locane <a.locane@eco-mind.eu>
2020-10-12 18:55:46 +0200

    added version check

--- vars/e4tDeployDockerImage.groovy
+++ vars/e4tDeployDockerImage.groovy
@@ -1,6 +1,25 @@
 void call(String repoName, String localTag, String remoteHost) {
     e4tExecute("""
         dep include \$DEP_SHELL_COMMON_PACKAGE \$DEP_SHELL_COMMON_VERSION projects ;\\
-        docker_push $repoName $localTag $remoteTag $remoteHost ;\\
+        branch=\$(git_branch) ;\\
+        version=\$(project_version) ;\\
+        case "\$branch" in \\
+            master) \\
+                remoteTag=\$version ;\\
+                ;; \\
+            *-rc*) \\
+                remoteTag="qa-\$version" ;\\
+                ;; \\
+            develop) \\
+                commitId=\$(git_commit) ;\\
+                remoteTag="lab-\$version-\$commitId" ;\\
+                ;; \\
+            *) \\
+                echo "skipping docker image deploy (branch=\$branch)" ;\\
+                ;;
+        esac ;\\
+        if [ -z "\$remoteTag" ]; then \\
+            docker_push $repoName $localTag \$remoteTag $remoteHost ;\\
+        fi ;\\
     """)
 }
