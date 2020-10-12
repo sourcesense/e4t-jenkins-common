b50117ce5f94b106f94487de5ffabc3e434552f6
Antonio Locane <a.locane@eco-mind.eu>
2020-10-12 17:47:10 +0200

    added e4tExecute

--- vars/e4tCheckVersion.groovy
+++ vars/e4tCheckVersion.groovy
@@ -1,11 +1,8 @@
 void call() {
     echo "Branch: ${BRANCH_NAME}"
     echo "Project: ${PROJECT_NAME}"
-    sh(script: '''
-        /bin/bash -c ' \\
-            . /app/dep-bootstrap.sh ;\\
-            dep include log2/shell-common 0.1.0-SNAPSHOT projects ;\\
-            check_version ;\\
-        '
-    ''')
+    e4tExecute(script: """
+        dep include \$DEP_SHELL_COMMON_PACKAGE \$DEP_SHELL_COMMON_VERSION projects ;\\
+        check_version ;\\
+    """)
 }
--- vars/e4tDeployDockerImage.groovy
+++ vars/e4tDeployDockerImage.groovy
@@ -1,9 +1,6 @@
 void call(String repoName, String localTag, String remoteTag, String remoteHost) {
-    sh(script: """
-        /bin/bash -c ' \\
-            . /app/dep-bootstrap.sh ;\\
-            dep include log2/shell-common 0.1.0-SNAPSHOT projects ;\\
-            docker_push $repoName $localTag $remoteTag $remoteHost ;\\
-        '
+    e4tExecute(script: """
+        dep include \$DEP_SHELL_COMMON_PACKAGE \$DEP_SHELL_COMMON_VERSION projects ;\\
+        docker_push $repoName $localTag $remoteTag $remoteHost ;\\
     """)
 }
