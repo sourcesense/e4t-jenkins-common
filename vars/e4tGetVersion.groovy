b9868680d3be186ef89480bfc11437deeb65c353
Lorenzo Gallucci <l.gallucci@eco-mind.eu>
2020-11-02 18:42:27 +0100

    Fixed: fixes non-working reference in getVersion

--- vars/e4tGetVersion.groovy
+++ vars/e4tGetVersion.groovy
@@ -2,7 +2,7 @@ void call() {
     echo "Branch: ${BRANCH_NAME}"
     echo "Project: ${PROJECT_NAME}"
     e4tDepScript("""
-        dep include ${e4tDepScript.shellCommon} projects ;\\
+        dep include EcoMind/git-common projects ;\\
         project_version ;\\
     """)
 }
