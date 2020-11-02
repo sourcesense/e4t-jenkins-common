8bc02d000f68a453865a15fa4a68cd3ddb2a3c8f
Lorenzo Gallucci <l.gallucci@eco-mind.eu>
2020-11-02 18:43:56 +0100

    Fixed: fixes bad reference to sh module

--- vars/e4tGetVersion.groovy
+++ vars/e4tGetVersion.groovy
@@ -2,7 +2,7 @@ void call() {
     echo "Branch: ${BRANCH_NAME}"
     echo "Project: ${PROJECT_NAME}"
     e4tDepScript("""
-        dep include EcoMind/git-common projects ;\\
+        dep include EcoMind/git-common versions ;\\
         project_version ;\\
     """)
 }
