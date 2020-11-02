7d7496a454ed069bdf3adb8e00e5aadfe2315d15
Lorenzo Gallucci <l.gallucci@eco-mind.eu>
2020-11-02 18:30:28 +0100

    Added: adds e4tGetVersion utility

---
+++ vars/e4tGetVersion.groovy
@@ -0,0 +1,8 @@
+void call() {
+    echo "Branch: ${BRANCH_NAME}"
+    echo "Project: ${PROJECT_NAME}"
+    e4tDepScript("""
+        dep include ${e4tDepScript.shellCommon} projects ;\\
+        project_version ;\\
+    """)
+}
