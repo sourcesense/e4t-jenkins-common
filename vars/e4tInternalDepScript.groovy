833a78c71e335cc0de7c24cecd9edda692577791
Antonio Locane <a.locane@eco-mind.eu>
2021-01-26 17:17:02 +0100

    updated dep versions

--- vars/e4tInternalDepScript.groovy
+++ vars/e4tInternalDepScript.groovy
@@ -3,11 +3,11 @@
 def call(String script, String result) {
     sh(script: """
         /bin/bash -c ' \\
-            . /app/dep-bootstrap.sh 0.4.1 ;\\
-            dep define log2/shell-common:0.2.0 ;\\
-            dep define EcoMind/git-common:0.3.0 ;\\
+            . /app/dep-bootstrap.sh 0.5.1 ;\\
+            dep define log2/shell-common:0.3.1 ;\\
+            dep define EcoMind/git-common:0.3.1 ;\\
             dep define EcoMind/container-common:0.1.0 ;\\
-            dep define EcoMind/k8s-common:0.1.0 ;\\
+            dep define EcoMind/k8s-common:0.2.1 ;\\
             $script
         '
     """,
