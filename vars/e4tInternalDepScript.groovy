fdb8d8b412a7d691f298b8c4134125dbd9b23391
Antonio Fragola <a.fragola@eco-mind.eu>
2021-02-24 14:29:49 +0100

    downgraded shell-common to 0.2.0 to avoid version conflicts

--- vars/e4tInternalDepScript.groovy
+++ vars/e4tInternalDepScript.groovy
@@ -4,7 +4,7 @@ def call(String script, String result) {
     sh(script: """
         /bin/bash -c ' \\
             . /app/dep-bootstrap.sh 0.5.1 ;\\
-            dep define log2/shell-common:0.3.1 ;\\
+            dep define log2/shell-common:0.2.0 ;\\
             dep define EcoMind/git-common:0.3.1 ;\\
             dep define EcoMind/container-common:0.1.0 ;\\
             dep define EcoMind/k8s-common:0.2.1 ;\\
