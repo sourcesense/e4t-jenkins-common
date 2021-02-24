fab1fc3e19c415558eca8f0d897d4be6835b0c1f
Antonio Fragola <a.fragola@eco-mind.eu>
2021-02-24 15:55:03 +0100

    restored shell-common to 0.3.1 and moved k8s-common to 0.2.3

--- vars/e4tInternalDepScript.groovy
+++ vars/e4tInternalDepScript.groovy
@@ -4,10 +4,10 @@ def call(String script, String result) {
     sh(script: """
         /bin/bash -c ' \\
             . /app/dep-bootstrap.sh 0.5.1 ;\\
-            dep define log2/shell-common:0.2.0 ;\\
+            dep define log2/shell-common:0.3.1 ;\\
             dep define EcoMind/git-common:0.3.1 ;\\
             dep define EcoMind/container-common:0.1.0 ;\\
-            dep define EcoMind/k8s-common:0.2.1 ;\\
+            dep define EcoMind/k8s-common:0.2.3 ;\\
             $script
         '
     """,
