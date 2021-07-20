8cb0f557678cceecafc88d32c8579118bcf750f1
Antonio Fragola <a.fragola@eco-mind.eu>
2021-07-20 10:38:38 +0200

    upgraded git-common to 0.4.1

--- vars/e4tInternalDepScript.groovy
+++ vars/e4tInternalDepScript.groovy
@@ -5,7 +5,7 @@ def call(String script, String result) {
         /bin/bash -c ' \\
             . /app/dep-bootstrap.sh 0.5.1 ;\\
             dep define log2/shell-common:0.3.1 ;\\
-            dep define EcoMind/git-common:0.3.1 ;\\
+            dep define EcoMind/git-common:0.4.1 ;\\
             dep define EcoMind/container-common:0.1.0 ;\\
             dep define EcoMind/k8s-common:0.2.3 ;\\
             $script
